e994ed7d5aef3ee765bcd038d95fc3dc55d26448
Amayagod14 <149508957+Amayagod14@users.noreply.github.com>
2025-01-21 17:30:11 -0500

    Add files via upload

--- poligrow_paz_y_salvo/config_mail.php
+++ poligrow_paz_y_salvo/config_mail.php
@@ -2,8 +2,14 @@
 // config_mail.php
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_USER', 'sebascata2005@gmail.com'); 
-define('SMTP_PASS', 'ffev jouu vbsb endg'); // Deberás generar una contraseña de aplicación en Gmail
+define('SMTP_PASS', 'ffev jouu vbsb endg'); // Contraseña de aplicación de Gmail
 define('SMTP_PORT', 465);
 define('SMTP_FROM', 'sebascata2005@gmail.com');
 define('SMTP_NAME', 'Sistema de Paz y Salvos Poligrow');
-define('IT_EMAIL', 'lider.it@poligrow.com');
+
+// Array de destinatarios
+define('IT_EMAILS', [
+    'lider.it@poligrow.com',
+    'otro.correo@dominio.com',
+    'soporte@poligrow.com',
+]);
